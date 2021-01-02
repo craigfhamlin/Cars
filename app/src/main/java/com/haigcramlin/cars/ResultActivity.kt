b0d68be16dc51a0f391e2ed7018f956c3db77149
CraigHamlin <haigcramlin@gmail.com>
2021-01-01 20:56:51 -0400

    new names for result_activity

--- app/src/main/java/com/haigcramlin/cars/ResultActivity.kt
+++ app/src/main/java/com/haigcramlin/cars/ResultActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TableLayout
 
 class ResultActivity : AppCompatActivity() {
     lateinit var buttonGoToMain: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        val tableLayout = findViewById<TableLayout>(R.id.table_layout)
         buttonGoToMain = findViewById(R.id.Return1)
         buttonGoToMain.setOnClickListener {
             val intent2: Intent = Intent(applicationContext, MainActivity::class.java)
